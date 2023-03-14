64b954ccd21e2abf183cd694fcbe17430cd24ca9
Miklos Herald Narra <miklos.herald@gmail.com>
2023-03-14 13:25:52 +0800

    Changes android store version

--- src/android.ts
+++ src/android.ts
@@ -1,25 +1,24 @@
 export const getAndroidVersion = async (storeURL = '') => {
-  if (
-    !storeURL.match(
-      /^https?:\/\/play\.google\.com\/store\/apps\/details\?id=[0-9a-zA-Z.]+/
-    )
-  ) {
-    throw new Error('androidStoreURL is invalid.');
-  }
-
-  const response = await fetch(storeURL).then((r) => {
-    if (r.status === 200) {
-      return r.text();
+    if (!storeURL.match(/^https?:\/\/play\.google\.com\/store\/apps\/details\?id=[0-9a-zA-Z.]+/)) {
+        throw new Error('androidStoreURL is invalid.');
     }
+    const response = await fetch(storeURL).then((r) => {
+        if (r.status === 200) {
+            return r.text();
+        }
+        throw new Error('androidStoreURL is invalid.');
+    });
 
-    throw new Error('androidStoreURL is invalid.');
-  });
 
-  const matches = response.match(/\[\[\[['"]((\d+\.)+\d+)['"]\]\],/);
+    // console.log('responsexx', response)
 
-  if (!matches) {
-    throw new Error("can't get android app version.");
-  }
-
-  return matches[1];
+    // const matches = response.match(/\[\[\[['"]((\d+\.)+\d+)['"]\]\],/);
+    const matches = response.match(/\([0-9]+\),/g);
+    if (!matches) {
+        throw new Error("can't get android app version.");
+    }
+    // return matches[1];
+    // console.log('matchesxx', matches)
+    
+    return String(matches[1].slice(1, -2))
 };
